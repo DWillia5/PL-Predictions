a772292f670c10b95808cf8f6f25bf29d1c98558
DWillia5 <daryl.williams@o2.com>
2013-01-21 15:38:34 +0000

    Made Scoring Files

--- lib/points_scoring/points_scoring.rb
+++ lib/points_scoring/points_scoring.rb
@@ -2,8 +2,10 @@ class MyNewClass
 
   def mymethod(user_predictions)
     user_predictions.each do |predictions|
-      do something with the scores
+      do "3 points for correct score"
     end
+      do "1 point for right result"
+    end    
   end
 
 end
